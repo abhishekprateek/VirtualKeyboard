181ebd60a9eb231964e8c31771e9fc410e6df8d0
surab <surab@abhip-desktop3.redmond.corp.microsoft.com>
2018-06-15 16:57:52 -0700

    Add json test cases

--- src/src/VirtualKeyboard.java
+++ src/src/VirtualKeyboard.java
@@ -1,13 +1,23 @@
 package src;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 import org.json.*;
 
-public class VirtualKeyboard {
-
-	public static void main(String[] args) 
+public class VirtualKeyboard 
+{
+	private static ILogger logger = new Logger();
+	
+	public static void main(String[] args) throws IOException 
 	{
+		// TODO: do parameter validations for args
 		
+		if (args.length != 2)
+		{
+			logger.Log("Incorrect number of arguments");
+		}
 		
-
+		String fileName = args[1];
 	}
-
 }
