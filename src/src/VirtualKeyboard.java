ee121ce4dde594aee5e69f9cc5cd84d92563ed23
surab <surab@abhip-desktop3.redmond.corp.microsoft.com>
2018-06-15 16:10:05 -0700

    Initial commit

---
+++ src/src/VirtualKeyboard.java
@@ -0,0 +1,12 @@
+package src;
+
+public class VirtualKeyboard {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello virtual keyboard!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
